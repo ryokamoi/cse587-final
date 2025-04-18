fd433fecdb7e5a1fc984619c5a9daabf27d34e12
ryokamoi <rfk5465@psu.edu>
2025-04-17 22:06:36 -0400

    fixed dataset/evaluation_dataset/extraction=Llama-3.3-70B-Instruct/evaluation.jsonl

--- src/training/preprocess_dataset.py
+++ src/training/preprocess_dataset.py
@@ -42,75 +42,77 @@ def get_chat_template_in_sharegpt(role: str, content: str) -> dict:
 def main():
     args = ResearchQuestionExtractionTap().parse_args()
 
-    # load extracted research questions and approaches
-    extracted_path = get_extracted_research_question_model_responses_path(
-        model_name=args.model_name, split="train"
-    )
-    with open(extracted_path, "r") as f:
-        raw_extracted_research_questions = [
-            json.loads(line) for line in f.readlines()
-        ]
-    
-    # preprocess research questions and approaches
-    processed_research_questions = []
-    for example in raw_extracted_research_questions:
-        if "This work proposes a new method: no" in example["output"]:
-            # skip the example
-            continue
-
-        processed_example = preprocess_extracted_research_questions_and_approaches(
-            example
+    for split in ["train", "test"]:
+        # load extracted research questions and approaches
+        extracted_path = get_extracted_research_question_model_responses_path(
+            model_name=args.model_name, split=split
         )
-        processed_research_questions.append(processed_example)
-    
-    evaluation_dataset_path = get_evaluation_dataset_path(
-        model_name=args.model_name
-    )
-    evaluation_dataset_path.parent.mkdir(parents=True, exist_ok=True)
-    with open(evaluation_dataset_path, "w") as f:
-        for example in processed_research_questions:
-            f.write(json.dumps(example) + "\n")
+        with open(extracted_path, "r") as f:
+            raw_extracted_research_questions = [
+                json.loads(line) for line in f.readlines()
+            ]
+        
+        # preprocess research questions and approaches
+        processed_research_questions = []
+        for example in raw_extracted_research_questions:
+            if "This work proposes a new method: no" in example["output"]:
+                # skip the example
+                continue
 
-    # convert into ShareGPT format
-    share_gpt_format_dataset = []
-    for example in processed_research_questions:
-        conversations = []
-        conversations.append(get_chat_template_in_sharegpt(
-            role="human",
-            content=example["research_question"]
-        ))
-        conversations.append(get_chat_template_in_sharegpt(
-            role="gpt",
-            content=example["approach"]
-        ))
-        share_gpt_format_dataset.append({"conversations": conversations})
-    
-    # save to file
-    output_path = sharegpt_dataset_dir / \
-        args.model_name.split("/")[-1] / "train.jsonl"
-    output_path.parent.mkdir(parents=True, exist_ok=True)
+            processed_example = preprocess_extracted_research_questions_and_approaches(
+                example
+            )
+            processed_research_questions.append(processed_example)
+        
+        if split == "test":
+            evaluation_dataset_path = get_evaluation_dataset_path(
+                model_name=args.model_name
+            )
+            evaluation_dataset_path.parent.mkdir(parents=True, exist_ok=True)
+            with open(evaluation_dataset_path, "w") as f:
+                for example in processed_research_questions:
+                    f.write(json.dumps(example) + "\n")
+        else:  # train
+            # convert into ShareGPT format
+            share_gpt_format_dataset = []
+            for example in processed_research_questions:
+                conversations = []
+                conversations.append(get_chat_template_in_sharegpt(
+                    role="human",
+                    content=example["research_question"]
+                ))
+                conversations.append(get_chat_template_in_sharegpt(
+                    role="gpt",
+                    content=example["approach"]
+                ))
+                share_gpt_format_dataset.append({"conversations": conversations})
+            
+            # save to file
+            output_path = sharegpt_dataset_dir / \
+                args.model_name.split("/")[-1] / "train.jsonl"
+            output_path.parent.mkdir(parents=True, exist_ok=True)
 
-    with open(output_path, "w") as f:
-        for example in share_gpt_format_dataset:
-            f.write(json.dumps(example) + "\n")
-    print(f"Saved to {output_path}")
+            with open(output_path, "w") as f:
+                for example in share_gpt_format_dataset:
+                    f.write(json.dumps(example) + "\n")
+            print(f"Saved to {output_path}")
 
-    # update dataset_info.json in llama factory
-    dataset_info_path = llama_factory_dir / "data/dataset_info.json"
-    with open(dataset_info_path, "r") as f:
-        dataset_info = json.load(f)
+            # update dataset_info.json in llama factory
+            dataset_info_path = llama_factory_dir / "data/dataset_info.json"
+            with open(dataset_info_path, "r") as f:
+                dataset_info = json.load(f)
 
-    dataset_info[dataset_name] = {
-        "file_name": str(Path("../../cse587-final" / output_path)),
-        "formatting": "sharegpt",
-        "columns": {
-            "messages": "conversations",
-        }
-    }
+            dataset_info[dataset_name] = {
+                "file_name": str(Path("../../cse587-final" / output_path)),
+                "formatting": "sharegpt",
+                "columns": {
+                    "messages": "conversations",
+                }
+            }
 
-    with open(dataset_info_path, "w") as f:
-        json.dump(dataset_info, f, indent=2)
-    print(f"Updated {dataset_info_path} with {dataset_name} dataset info.")
+            with open(dataset_info_path, "w") as f:
+                json.dump(dataset_info, f, indent=2)
+            print(f"Updated {dataset_info_path} with {dataset_name} dataset info.")
 
 if __name__ == "__main__":
     main()
